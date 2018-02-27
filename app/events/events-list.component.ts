bd65d43b5f0b722601ee0bd3a5e2da1d714f250f
Anthony <nin0@nin0.com>
2018-02-27 16:32:25 -0500

    updates

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -2,16 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 import { EventThumbnailComponent } from './event-thumbnail.component';
 import { EventService } from './shared/event.service'; 
+import { ToastrService } from '../common/toastr.service';
 
-@Component({
-    selector: 'events-list',
+@Component({    
     template: `
     <div>
         <h1>Upcoming Angular 2 Events</h1>
         <hr/>
         <div class='row'>
             <div *ngFor='let event of events' class='col-md-5'>
-                <event-thumbnail [event]='event'></event-thumbnail>
+                <event-thumbnail (click)='handleThumbnailClick(event.name)' [event]='event'></event-thumbnail>
             </div>
         </div>
     </div>
@@ -22,11 +22,15 @@ export class EventsListComponent implements OnInit {
 
     events: any[];
 
-    constructor(private eventService: EventService) {
+    constructor(private eventService: EventService, private toastrService: ToastrService) {
         
     }
 
     ngOnInit() {
         this.events = this.eventService.getEvents();
+    }
+
+    handleThumbnailClick(eventName) {
+        this.toastrService.success(eventName);
     }
 }
