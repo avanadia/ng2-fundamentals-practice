6566994850c3fb403bc2690a38c60a99c46b669d
Anthony <nin0@nin0.com>
2018-02-27 12:40:17 -0500

    updates

--- app/events/event-thumbnail.component.js
+++ app/events/event-thumbnail.component.js
@@ -21,8 +21,8 @@ __decorate([
 EventThumbnailComponent = __decorate([
     core_1.Component({
         selector: 'event-thumbnail',
-        template: "\n    <div class='well hoverwell thumbnail'>\n        <h2>{{event.name}}</h2>\n        <div>Date: {{event.date}}</div>\n        <div>Time: {{event.time}}</div>\n        <div>Price: {{event.price | currency : USD:true}}</div>\n        <div>\n            <span>Location: {{event.location.address}}</span>\n            <span class='pad-left'>{{event.location.city}}, {{event.location.country}}</span>\n        </div>\n    \n    </div>\n    ",
-        styles: ["\n        .pad-left { margin-left: 10px; }\n        .well { color: #bbb; }\n    "]
+        template: "\n    <div class='well hoverwell thumbnail'>\n        <h2>{{event?.name}}</h2>\n        <div>Date: {{event?.date}}</div>\n        <div>Time: {{event?.time}}</div>\n        <div>Price: {{event?.price | currency : USD:true}}</div>\n        <div *ngIf='event?.location'>\n            <span>Location: {{event?.location?.address}}</span>\n            <span class='pad-left'>{{event?.location?.city}}, {{event?.location?.country}}</span>\n        </div>\n        <div *ngIf='event?.onlineUrl'>\n        Online Url: {{ event?.onlineUrl }}\n        </div>\n    </div>\n    ",
+        styles: ["\n        .thumbnail { min-height: 210px; }\n        .pad-left { margin-left: 10px; }\n        .well { color: #bbb; }\n    "]
     }),
     __metadata("design:paramtypes", [])
 ], EventThumbnailComponent);
--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,18 +4,21 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     selector: 'event-thumbnail',
     template: `
     <div class='well hoverwell thumbnail'>
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: {{event.price | currency : USD:true}}</div>
-        <div>
-            <span>Location: {{event.location.address}}</span>
-            <span class='pad-left'>{{event.location.city}}, {{event.location.country}}</span>
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div>Time: {{event?.time}}</div>
+        <div>Price: {{event?.price | currency : USD:true}}</div>
+        <div *ngIf='event?.location'>
+            <span>Location: {{event?.location?.address}}</span>
+            <span class='pad-left'>{{event?.location?.city}}, {{event?.location?.country}}</span>
         </div>
-    
+        <div *ngIf='event?.onlineUrl'>
+        Online Url: {{ event?.onlineUrl }}
+        </div>
     </div>
     `,
     styles: [`
+        .thumbnail { min-height: 210px; }
         .pad-left { margin-left: 10px; }
         .well { color: #bbb; }
     `]
