459760206c5af4305360bd73b402a044780de5b5
Anthony <nin0@nin0.com>
2018-03-04 14:02:00 -0500

    working on integrated tests but it's not really working

--- app/events/event-details/upvote.component.js
+++ app/events/event-details/upvote.component.js
@@ -5,10 +5,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var core_1 = require("@angular/core");
 var UpvoteComponent = /** @class */ (function () {
     function UpvoteComponent() {
@@ -25,18 +22,14 @@ var UpvoteComponent = /** @class */ (function () {
         this.vote.emit({});
     };
     __decorate([
-        core_1.Input(),
-        __metadata("design:type", Number)
-    ], UpvoteComponent.prototype, "count", void 0);
+        core_1.Input()
+    ], UpvoteComponent.prototype, "count");
     __decorate([
-        core_1.Input(),
-        __metadata("design:type", Object),
-        __metadata("design:paramtypes", [Object])
-    ], UpvoteComponent.prototype, "voted", null);
+        core_1.Input()
+    ], UpvoteComponent.prototype, "voted");
     __decorate([
-        core_1.Output(),
-        __metadata("design:type", Object)
-    ], UpvoteComponent.prototype, "vote", void 0);
+        core_1.Output()
+    ], UpvoteComponent.prototype, "vote");
     UpvoteComponent = __decorate([
         core_1.Component({
             selector: 'upvote',
@@ -47,4 +40,3 @@ var UpvoteComponent = /** @class */ (function () {
     return UpvoteComponent;
 }());
 exports.UpvoteComponent = UpvoteComponent;
-//# sourceMappingURL=upvote.component.js.map
